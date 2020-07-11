df6155f92979df38d9bace5fc3bc8ce9881f5d69
oscar02r <oscary.ramirez@gmail.com>
2020-07-11 02:53:57 +0200

    Added background to container button

--- designcalculate/lib/src/pages/mainScreen.dart
+++ designcalculate/lib/src/pages/mainScreen.dart
@@ -33,9 +33,10 @@ class MainScreen extends StatelessWidget {
                 bottomLeft: Radius.zero,
                 bottomRight: Radius.zero),
             child: Container(
+                 height: double.infinity,
+                 width:double.infinity,
+                color:  Color.fromRGBO(249, 251, 251, 0.5), 
                 padding: EdgeInsets.all(20.0),
-                color: Colors.blue,
-                width: double.infinity,
                 child: Column(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: <Widget>[
@@ -46,86 +47,89 @@ class MainScreen extends StatelessWidget {
                       _fifthRow(),
                     ]))));
   }
-}
 
-Widget _button(String title, int r, int g, int b, double o1, double o2) {
-  Color _background = Color.fromRGBO(219, 146, 124, o2);
-  _background = title != '='
-      ? Color.fromRGBO(219, 146, 124, o2)
-      : Color.fromRGBO(118, 16, 0, o2);
+  Widget _button(String title, int r, int g, int b, double o1, double o2) {
+    Color _background = Color.fromRGBO(219, 146, 124, o2);
+    _background = title != '='
+        ? Color.fromRGBO(219, 146, 124, o2)
+        : Color.fromRGBO(118, 16, 0, o2);
 
-  return Container(
-      alignment: Alignment.center,
-      width: 70.0,
-      height: 50.0,
-      color: _background,
-      child: Text(
-        title,
-        style: TextStyle(
-            color: Color.fromRGBO(r, g, b, o1),
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold),
-      ));
-}
-
-Widget _firstRow() {
-  return Row(
-    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-    children: <Widget>[
-      _button('C', 249, 251, 251, 1.0, 1.0),
-      _button('+/-', 249, 251, 251, 1.0, 1.0),
-      _button('%', 249, 251, 251, 1.0, 1.0),
-      _button('/', 249, 251, 251, 1.0, 1.0),
-    ],
-  );
-}
-
-Widget _secondRow() {
-  return Row(
-    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-    children: <Widget>[
-      _button('7', 118, 16, 0, 1.0, 0.0),
-      _button('8', 118, 16, 0, 1.0, 0.0),
-      _button('9', 118, 16, 0, 1.0, 0.0),
-      _button('X', 249, 251, 251, 1.0, 1.0),
-    ],
-  );
-}
-
-Widget _thirdRow() {
-  return Row(
-    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-    children: <Widget>[
-      _button('4', 118, 16, 0, 1.0, 0.0),
-      _button('5', 118, 16, 0, 1.0, 0.0),
-      _button('6', 118, 16, 0, 1.0, 0.0),
-      _button('-', 249, 251, 251, 1.0, 1.0),
-    ],
-  );
-}
-
-Widget _fouthRow() {
-  return Row(
-    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-    children: <Widget>[
-      _button('1', 118, 16, 0, 1.0, 0.0),
-      _button('2', 118, 16, 0, 1.0, 0.0),
-      _button('3', 118, 16, 0, 1.0, 0.0),
-      _button('+', 249, 251, 251, 1.0, 1.0),
-    ],
-  );
-}
-
-Widget _fifthRow() {
-  return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: <Widget>[
-        _button('0', 118, 16, 0, 1.0, 0.0),
-        SizedBox(
+    return ClipRRect(
+          borderRadius: BorderRadius.circular(10.0),
+          child: Container(
+          alignment: Alignment.center,
           width: 70.0,
           height: 50.0,
-        ),
-        _button(',', 118, 16, 0, 1.0, 0.0),
-        _button('=', 249, 251, 251, 1.0, 1.0)
-      ]);
+          color: _background,
+          child: Text(
+            title,
+            style: TextStyle(
+                color: Color.fromRGBO(r, g, b, o1),
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold),
+          )),
+    );
+  }
+
+  Widget _firstRow() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        _button('C', 249, 251, 251, 1.0, 1.0),
+        _button('+/-', 249, 251, 251, 1.0, 1.0),
+        _button('%', 249, 251, 251, 1.0, 1.0),
+        _button('/', 249, 251, 251, 1.0, 1.0),
+      ],
+    );
+  }
+
+Widget _secondRow() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        _button('7', 118, 16, 0, 1.0, 0.0),
+        _button('8', 118, 16, 0, 1.0, 0.0),
+        _button('9', 118, 16, 0, 1.0, 0.0),
+        _button('X', 249, 251, 251, 1.0, 1.0),
+      ],
+    );
+  }
+
+  Widget _thirdRow() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        _button('4', 118, 16, 0, 1.0, 0.0),
+        _button('5', 118, 16, 0, 1.0, 0.0),
+        _button('6', 118, 16, 0, 1.0, 0.0),
+        _button('-', 249, 251, 251, 1.0, 1.0),
+      ],
+    );
+  }
+
+  Widget _fouthRow() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        _button('1', 118, 16, 0, 1.0, 0.0),
+        _button('2', 118, 16, 0, 1.0, 0.0),
+        _button('3', 118, 16, 0, 1.0, 0.0),
+        _button('+', 249, 251, 251, 1.0, 1.0),
+      ],
+    );
+  }
+
+  Widget _fifthRow() {
+    return Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          _button('0', 118, 16, 0, 1.0, 0.0),
+          SizedBox(
+            width: 70.0,
+            height: 50.0,
+          ),
+          _button(',', 118, 16, 0, 1.0, 0.0),
+          _button('=', 249, 251, 251, 1.0, 1.0)
+        ]);
+  }
 }
